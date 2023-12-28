123c0de5fdd41a164c7cda8303c782c63809427a
Vinay <vinaychougale9222@gmail.com>
2023-12-28 16:31:06 +0530

    day 2

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Vinay";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counting App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days new day with $name !!"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:stock/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-        child:Center(
-        child:Text("Welcome to new day"),
-    ),
-    ),
+      home: HomePage(),
     );
   }
 }
