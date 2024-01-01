74f962ad3a826e4f4c1d27336dfd3931dbde8092
Vinay <vinaychougale9222@gmail.com>
2024-01-01 20:22:53 +0530

    Day5 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       darkTheme: ThemeData(brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
       // initialRoute: '/home',
       routes: {
         '/': (context) => LoginPage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
-import 'dart:html';
+// import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:stock/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -11,38 +12,44 @@ class LoginPage extends StatelessWidget {
         color: Colors.white,
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+            ),
             Image.asset(
               "assets/images/login_page.png",
               fit: BoxFit.cover,
             ),
-            Text("Welcome",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline1!
-                    .copyWith(fontWeight: FontWeight.bold)),
+            Padding(
+              padding: const EdgeInsets.all(16),
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
             SizedBox(
-              height: 20,
+              height: 9,
             ),
             Padding(
                 padding: const EdgeInsets.all(16),
                 child: Column(
                   children: [
                     TextField(
-                        obscureText: true,
                         decoration: InputDecoration(
                             hintText: "Enter Username", labelText: "Username")),
                     TextField(
+                        obscureText: true,
                         decoration: InputDecoration(
                             hintText: "Enter Password", labelText: "Password")),
                     SizedBox(
                       height: 20,
                     ),
                     ElevatedButton(
-                        onPressed: () {
-                          print("Hi Vinay");
-                        },
-                        child: Text("Login"),
-                        style: TextButton.styleFrom())
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                    )
                   ],
                 ))
           ],
