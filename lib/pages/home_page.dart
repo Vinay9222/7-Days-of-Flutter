5147a0a2aa747fc77c1aca626b8c57cc856cb906
Vinay <vinaychougale9222@gmail.com>
2024-01-06 19:00:47 +0530

    Day14 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -35,28 +35,78 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Catalog App",
-          style: TextStyle(color: Colors.black),
-        ),
+    //   appBar: AppBar(
+    //     title: Text(
+    //       "Catalog App",
+    //       style: TextStyle(color: Colors.black),
+    //     ),
+    //   ),
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(16.0),
+    //     // child: ListView.builder(
+    //     //   itemCount: CatalogModel.items.length,
+    //     //   itemBuilder: (context, index) {
+    //     //     return ItemWidget(
+    //     //       item: CatalogModel.items[index],
+    //     //     );
+    //     //   },
+    //     // ),
+    //     child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+    //         ? ListView.builder(
+    //             itemCount: CatalogModel.items.length,
+    //             itemBuilder: (context, index) => ItemWidget(
+    //               item: CatalogModel.items[index],
+    //             ),
+    //           )
+    //         : Center(
+    //             child: CircularProgressIndicator(),
+    //           ),
+    //   ),
+    appBar: AppBar(
+        title: Text("Catalog App"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        // child: ListView.builder(
-        //   itemCount: CatalogModel.items.length,
-        //   itemBuilder: (context, index) {
-        //     return ItemWidget(
-        //       item: CatalogModel.items[index],
-        //     );
-        //   },
-        // ),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items[index],
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
                 ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        child: Image.network(
+                          item.image,
+                        ),
+                        footer: Container(
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ));
+                },
+                itemCount: CatalogModel.items.length,
               )
             : Center(
                 child: CircularProgressIndicator(),
